e5ff8837ff113cc2146f23df2934f4c07a9470e0
Adam Lee <AdamBLee@users.noreply.github.com>
2021-06-17 18:03:05 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -37,21 +37,46 @@ function spin_left () {
     basic.pause(spin_time)
     Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
 }
+radio.onReceivedStringDeprecated(function (receivedString) {
+    item = receivedString
+    if (item.compare("stick_up") == 0) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
+    } else if (item.compare("stick_down") == 0) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Back)
+    } else if (item.compare("stick_left") == 0) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Left)
+    } else if (item.compare("stick_right") == 0) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Right)
+    } else if (item.compare("stick_press") == 0) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+    } else if (item.compare("button_red") == 0) {
+        Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+    } else if (item.compare("button_green") == 0) {
+        Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+    } else if (item.compare("button_blue") == 0) {
+        Tinybit.RGB_Car_Big(Tinybit.enColor.Blue)
+    } else if (item.compare("button_yellow") == 0) {
+        Tinybit.RGB_Car_Big(Tinybit.enColor.Yellow)
+    } else {
+    	
+    }
+})
 function turn_right () {
     Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Right, forward_right_speed)
     basic.pause(spin_time - 70)
     Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
 }
+let item = ""
 let forward_left_speed = 0
 let forward_right_speed = 0
 let spin = 0
 let spin_time = 0
-basic.showIcon(IconNames.Duck)
+let radio_group = 1
+basic.showNumber(radio_group)
+radio.setGroup(radio_group)
+radio.setTransmitPower(7)
 spin_time = 260
 spin = 140
 forward_right_speed = 210
 forward_left_speed = 255
 Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
-basic.forever(function () {
-	
-})
